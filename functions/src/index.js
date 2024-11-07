82299ffb7bc224b9b737a6096da01e6d7bb4daa4
Eerina Haque <eerinahaque@gmail.com>
2024-11-06 16:20:59 -0800

    const add user func with client Firebase JS SDK

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,19 +1,38 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-admin.initializeApp();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDsqE8t5QnzfcQuSU2D2BKVGGOlIuj84Tk",
+  authDomain: "chorganizer-29aa5.firebaseapp.com",
+  projectId: "chorganizer-29aa5",
+  storageBucket: "chorganizer-29aa5.appspot.com",
+  messagingSenderId: "983388578449",
+  appId: "1:983388578449:web:c3e858e05b17e81245d530",
+  measurementId: "G-C5P42ZVWEE"
+};
 
-// Import functions
-const auth = require('./auth/auth');
-const houses = require('./houses/houses');
-const chores = require('./chores/chores');
-const notifications = require('./notifications/notifications');
-const storage = require('./storage/storage');
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// Export functions
-exports.createUser = auth.createUser;
-exports.createHouse = houses.createHouse;
-exports.addChore = chores.addChore;
-exports.rotateChores = chores.rotateChores;
-exports.sendNotification = notifications.sendNotification;
-exports.uploadProof = storage.uploadProof;
+// export default app;
+
+async function addUser() {
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+}
+
+export default addUser;
