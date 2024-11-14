2e94302aba598f2ef452ac9cd7cb1f3e53f82b46
madinguyen9 <mnguyen9@scu.edu>
2024-11-14 12:06:29 -0800

    Essentially the AuthScreen/Sign in page
    Launch page also adjusted to removed old Google Signin. Still need to fix App.tsx

--- Choreganizer/pages/Launch.tsx
+++ Choreganizer/pages/Launch.tsx
@@ -24,35 +24,7 @@ const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
 
 function Launch({ navigation }: { navigation: any }): React.JSX.Element {
-  // State to track if user is signed in
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
-  // Simulate the Google sign-in process (in the real app, connect to backend)
-  const handleGoogleSignIn = () => {
-    // For now, we simulate the sign-in process by updating the state
-    setIsSignedIn(true);
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential?.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-      console.log(errorCode, errorMessage, email, credential);
-    });
-  };
-
+ 
   return (
     <View style={{
         flex: 1,
